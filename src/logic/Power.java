18e7687b85d92156807a79319b4d7a62912ec9a9
Palakdeep Kaur <kaurpalak193@gmail.com>
2025-12-21 22:56:33 +0530

    Add files via upload

---
+++ src/logic/Power.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+
+public class Power{
+    // Leetcode problem link: https://leetcode.com/problems/powx-n/description/
+
+    public static double myPow(double x , int n){ 
+     long N = n; // Convert to long to avoid overflow
+        if (N == 0) return 1;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double half = myPow(x, (int)(N / 2));
+
+        if (N % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+
+    }
+
+    public static void main(String[] args){
+  double a =34.0005 ;
+        int b = -3 ;
+        System.out.println(Poweroptimal(a,b) );
+    }
+}
