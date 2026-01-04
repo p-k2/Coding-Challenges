0ad5127509223d43859a7e21c9764b867e561148
p-k2 <kaurpalak193@gmail.com>
2026-01-04 20:55:28 +0530

    Migratory Bird problem solved

---
+++ src/logic/MigratoryBirds.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+//Problem Link: https://www.hackerrank.com/challenges/migratory-birds/problem
+
+public class MigratoryBirds{
+
+    public static int migratoryBirds(List<Integer> arr) {
+        int[] freq = new int[6]; //bird IDs are 1 to 5
+
+        //Count frequencies
+        for(int num : arr){
+            freq[num]++;
+        }
+
+        int maxFreq =0;
+        int result = 0;
+
+        //Find bird with max frequency (smallest id if tie)
+        for( int i=1 ; i<=5 ; i++){
+          int currLargest = freq[i] ;
+          if(currLargest> maxFreq){
+            maxFreq= currLargest;
+            result =i ;
+          }
+        }
+        return result;
+    
+    }
+
+    public static void main(String[] args ){
+         List<Integer> ar = new ArrayList<Integer>() ;
+        ar.add(1) ;
+        ar.add(2) ;
+        ar.add(3) ;
+        ar.add(4) ;
+        ar.add(5) ;
+        ar.add(4) ;
+        ar.add(3) ;
+        ar.add(2) ;
+        ar.add(1) ;
+        ar.add(3) ;
+        ar.add(4) ;
+        System.out.println(migratoryBirds(ar));
+
+    }
+}
